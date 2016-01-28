da577efe29416ae4f6188f789d82081425a36ba9
Hanbing Guo <t800ghb@qq.com>
2016-01-28 23:14:09 +0800

    Keep main.py as a execution platform, so it will also empty

--- main.py
+++ main.py
@@ -5,52 +5,8 @@ Created on Thu Dec 31 12:41:35 2015
 @author: T800GHB
 """
 
-import basic_element.basic_operation as bo
-import basic_function.function_call as fc
-
 """
-This is a basic output
+This is a platform that be used to execute example.
+So, I will keep this file empty.
 """
-#bo.Basic_Print()
-"""
-This is a function of basic operation about list
-"""
-#bo.Basic_list()
-"""
-This is a function of basic operation about tuple
-"""
-#bo.Basic_tuple()
-
-"""
-This is a function about judgement.
-"""
-#bo.Basic_judge()
-
-"""
-This function could implement basic loop operation.
-"""
-#bo.Basic_loop()
-
-"""
-Dictionary
-"""
-#bo.Basic_dict()
-
-"""
-Set
-"""
-#bo.Basic_set()
-"""
-Function call
-"""
-#fc.call_basic()
-"""
-Type convertion function
-"""
-#fc.type_convert()
-
-"""
-Function parameter check and multi_return
-"""
-fc.controler()
 
