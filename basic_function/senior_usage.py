daed8702f856845fadf1ee17e253e76bcd2e8dab
Hanbing Guo <t800ghb@qq.com>
2016-01-31 11:12:28 +0800

    Add a example about map and reduce function

--- basic_function/senior_usage.py
+++ basic_function/senior_usage.py
@@ -9,6 +9,12 @@ Something like, functional programming, lambda function.
 
 def add(x, y, func):
     return func(x) + func(y)
+    
+def square(x):
+    return x*x
+    
+def combine(x, y):
+    return x*10 + y
 
 def high_order_function():
     """This script will show how to pass a function as a argument into another
@@ -22,4 +28,20 @@ def high_order_function():
     #Pass a function as a argument
     res = add(-5, 6, func)
     print('The result of addition is :' , res)
+    
+def map_reduce():
+    """This script will show how use a high-level function map and reduce
+    """
+    list_var = [0,1,2,3,4,5,6,7,8,9]
+    #Map require two argument as input, a function work on each element and data
+    res = map(square, list_var)
+    #The return value will be a iterator, so we need to convert to a list for display
+    print('The result of map is :', list(res))
+    from functools import reduce
+    #reduce function will work on two continuous element 
+    res = reduce(combine, list_var)
+    print('The result of reduce is :', res)
+    
+    
+
     
