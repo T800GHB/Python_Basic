3b6d5d11d979b83c2cf12727e687acbb9ed82e82
T800GHB <t800ghb@qq.com>
2016-02-28 16:20:20 +0800

    exception raise

--- basic_exception/exception.py
+++ basic_exception/exception.py
@@ -8,16 +8,29 @@ This file will show how to deal with exception.
 
 def error_demo():
     return 10 / 0
+    
+def error_raise(var):
+    #This will throw an exception and upper caller will handle it.
+    if var == 0:
+        raise ValueError('invalid value: %d' % var)
+    return 10 / var
 
 def run_demo():
     """
     This stript demostrate how to handle the exception
     """
     var = input('Input an number: ')
-    #If some error will occur in this procedure, put it into try block.
+    """
+    If some error will occur in this procedure, put it into try block.
+    Try block will stop at the exception occur, the rest commend will not
+    execute.In this block, if error_raise(0) make an exception, error_demo
+    will not execute.
+    """
     try:
         print('try...')        
         r = 10 / int(var)
+        #Catch the exception throwed.
+        error_raise(0)
         #Handle the exception happened in the called function.
         #So, you don't need to assign try block everywhere.
         #Just put try block on the spot where the exception may occur
