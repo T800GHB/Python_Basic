a207b5a28579ff3f1c6e434f2e8319530b8d3cda
T800GHB <t800ghb@qq.com>
2016-06-30 00:39:33 +0800

    unique operation on array. efficient but disorder

--- algorithm/unique.py
+++ algorithm/unique.py
@@ -52,15 +52,78 @@ def unique_ordered(a):
     a[head : len(a)] = []
     print(a)
     
+def unique_chaos(a):
+    '''
+    Reserve unique elements onto orignal array, 
+    but this kind of method could guarantee order.
+    This way is more efficient, because less copy.
+    '''
+    '''Situation is same as above one'''
+    cur = 1
+    for i in range(1,len(a)):
+        for j in range(cur):
+            if a[j] != 0:
+                if a[i] == a[j]:
+                    a[i] = 0
+        cur += 1
+    '''Copy element from rear to empty location'''
+    head = 0;
+    rear = len(a) - 1
+    while head < rear:
+        '''Head pointer find empty location'''
+        if a[head] == 0:
+            '''Rear index point to an empty location'''
+            if a[rear] == 0:
+                '''Find un-empty location at rear'''
+                while a[rear] == 0:
+                    rear -= 1
+                if head < rear:
+                    '''No conflict between head and rear, copy from rear'''
+                    a[head] = a[rear]
+                    rear -= 1
+                    head += 1
+                else:
+                    '''Conflicted, quit directly'''
+                    head = rear
+            else:
+                '''Rear is not empty'''
+                a[head] = a[rear]
+                rear -= 1
+                head += 1                
+        else:
+            '''Head pointer has not find empty location, keep searching'''
+            head += 1
+    count = head + 1
+    print('Number of element of is: ', count)
+    '''Delete empty element'''
+    a[count : len(a)] = []
+    print(a)
+    
 def demo():
     '''Test set'''
     a = [1,1,2,2,1,3,4,4,3,3,5,1,2,4,6,3]       #Normal
     b = [1,2,3,4]                               #All different
     c = [1,1,1,1]                               #All same
+    d = [7] 
+    a1 = a.copy()
+    b1 = b.copy()
+    c1 = c.copy()
+    d1 = d.copy()                                    #Only one
+    print('Orignal data')
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print('unique_ordered')
     unique_ordered(a)
     unique_ordered(b)
     unique_ordered(c)
-    
+    unique_ordered(d)                              
+    print('unique_chaos')
+    unique_chaos(a1)
+    unique_chaos(b1)
+    unique_chaos(c1)
+    unique_chaos(d1)
                 
     
             
