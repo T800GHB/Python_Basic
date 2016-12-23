afb1f7b6e95f02e51a7875496212ac822c3feb46
Hanbing Guo <t800ghb@qq.com>
2016-12-24 01:19:30 +0800

    add log demo about stand output and error record

--- basic_exception/log_record.py
+++ basic_exception/log_record.py
@@ -4,6 +4,7 @@ Created on Sun Feb 28 15:57:00 2016
 
 @author: T800GHB
 This file will show how to use log record important message.
+
 """
 
 import logging
@@ -21,4 +22,37 @@ def run_demo():
         logging.exception(e)
     print('END')
 
+def error_demo():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)    
+    # create a file handler    
+    handler = logging.FileHandler('error.log')    
+    handler.setLevel(logging.INFO)   
+    # create a logging format    
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+    handler.setFormatter(formatter)    
+    # add the handlers to the logger    
+    logger.addHandler(handler)    
+    logger.info('Hello, this is a error logging demo.')
+    try:
+        bar('0')
+    except Exception as e:
+        #Use exc_info=True to record call stack
+        logger.error('Incorrect usage: ', exc_info = True)
+        #Or use like this for exception
+        #logger.exception('Wrong usage: ')
+        
+def output_demo():
+    '''
+    The default level is warning, 
+    so the level higher than warning will sent out message.
+    The level order is 
+    CRITICAL > ERROR > WARNING > INFO > DEBUG > NOTSET
+    '''
+    logging.debug('debug message')  
+    logging.info('info message')  
+    logging.warning('warning message')  
+    logging.error('error message')  
+    logging.critical('critical message') 
+    
