dc58d1facebcd21c71878b46c3902b2e21065e7a
Andrew Green <t800ghb@qq.com>
2017-07-22 00:42:38 +0800

    first try for c extension

---
+++ c_extension/first_try/math_opt.cpp
@@ -0,0 +1,69 @@
+#include <Python.h>
+#include <iostream>
+using namespace std;
+
+static int add(int arg1, int arg2){
+    return arg1 + arg2;
+}
+
+static PyObject *
+math_system(PyObject *self, PyObject *args)
+{
+    int arg1;
+    int arg2;
+
+    if (!PyArg_ParseTuple(args, "ii", &arg1, &arg2))
+        return NULL;
+    int result = add(arg1, arg2);
+
+    return (PyObject*)Py_BuildValue("i", result);
+}
+
+static PyMethodDef MathMethods[] = {
+
+    {"add",  math_system, METH_VARARGS,
+     "Execute add operation."},
+
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+static struct PyModuleDef math_module = {
+   PyModuleDef_HEAD_INIT,
+   "math_opt",   /* name of module */
+   NULL, /* module documentation, may be NULL */
+   -1,       /* size of per-interpreter state of the module,
+                or -1 if the module keeps state in global variables. */
+   MathMethods
+};
+
+PyMODINIT_FUNC
+PyInit_math_opt(void)
+{
+    return PyModule_Create(&math_module);
+}
+
+int main(int argc, char *argv[])
+{
+    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+    if (program == NULL) {
+        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+        exit(1);
+    }
+
+    /* Add a built-in module, before Py_Initialize */
+    PyImport_AppendInittab("math_opt", PyInit_math_opt);
+
+    /* Pass argv[0] to the Python interpreter */
+    Py_SetProgramName(program);
+
+    /* Initialize the Python interpreter.  Required. */
+    Py_Initialize();
+
+    /* Optionally import the module; alternatively,
+       import can be deferred until the embedded script
+       imports it. */
+    PyImport_ImportModule("math_opt");
+
+    PyMem_RawFree(program);
+    return 0;
+}
---
+++ c_extension/first_try/run_extension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 22 00:37:24 2017
+
+@author: andrew
+"""
+
+import math_opt
+
+result = math_opt.add(3,4)
+
+print('First C extension result: ', result)
